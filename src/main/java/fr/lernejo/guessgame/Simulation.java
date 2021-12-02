b75a76c05cb0d27059fdc1285992b704221ce5b3
AnouarZayane <zayane@et.esiea.fr>
2021-12-02 10:37:09 +0100

    Commit Partie 5

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -3,9 +3,11 @@ package fr.lernejo.guessgame;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 
-    public class Simulation {
+public class Simulation {
 
         private final Logger logger = LoggerFactory.getLogger("simulation");
         private final Player player;  //TODO add variable type
@@ -39,14 +41,32 @@ import fr.lernejo.logger.LoggerFactory;
                 return false;
             }
         }
-            public void loopUntilPlayerSucceed()
+            public void loopUntilPlayerSucceed(long maxiteration)
             {
-                //TODO implement me
+                logger.log(("Début de la partie"));
+                long debut = System.currentTimeMillis();
+                int i=0;
                 boolean test = nextRound();
-                while (!test) {
+                while (!test)
+                {
                     test = nextRound();
+                    i++;
                 }
+                if (test && i<maxiteration)
+                {
+                    logger.log("Fini avant le maximum d'itérations : "+i);
+                }
+                else
+                {
+                    logger.log(("Fini après le maximum d'itérations" + i));
+                }
+                long fin = System.currentTimeMillis();
+                Timestamp timestamp=new Timestamp(fin-debut);
+                logger.log("Temps : "+new SimpleDateFormat("mm:ss.SSS").format(timestamp));
+
             }
-        }
+
+
+    }
 
 
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,8 +2,9 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
 
+    Logger logger = LoggerFactory.getLogger("launcher");
     public static Logger getLogger(String name)
     {
-      return new ConsoleLogger();
+      return new ContextualLogger(new FileLogger("C:/Users/user_pc/Desktop/Architecture/decouplig_java_training/decouplig_java_training/logs.txt"),name);
     }
 }
